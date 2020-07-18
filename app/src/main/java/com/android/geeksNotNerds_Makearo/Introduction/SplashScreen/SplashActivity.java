014217dceb93ee73b4171bfabc259454b92e9fcd
Niharika Kumar <99.niharikakumar@gmail.com>
2020-07-18 20:30:12 +0530

    Login UI

--- app/src/main/java/com/android/geeksNotNerds_Makearo/Introduction/SplashScreen/SplashActivity.java
+++ app/src/main/java/com/android/geeksNotNerds_Makearo/Introduction/SplashScreen/SplashActivity.java
@@ -1,8 +1,21 @@
 package com.android.geeksNotNerds_Makearo.Introduction.SplashScreen;
 
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.geeksNotNerds_Makearo.R;
 
 public class SplashActivity extends AppCompatActivity
 {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+    }
 }
