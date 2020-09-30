cc265102c4ba10ff80a53dbc9c1f7ca960092aa7
99nk <99.niharikakumar@gmail.com>
2020-09-30 21:56:26 +0530

    Designing the login activity via MVP architecture

--- app/src/main/java/com/android/geeksNotNerds_Makearo/Authentication/Login/MVP/LoginActivity.java
+++ app/src/main/java/com/android/geeksNotNerds_Makearo/Authentication/Login/MVP/LoginActivity.java
@@ -1,6 +1,33 @@
 package com.android.geeksNotNerds_Makearo.Authentication.Login.MVP;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+import com.android.geeksNotNerds_Makearo.R;
+
+import butterknife.BindView;
+
+public class LoginActivity extends AppCompatActivity implements LoginContract.view
+{
+    LoginContract.presenter presenter;
+    String access_token,return_email;
+
+    @BindView(R.id.loginPage_EmailText)
+    EditText mEmailLoginEditText;
+    @BindView(R.id.loginPage_enterPassword)
+    EditText mPasswordLoginEditText;
+    @BindView(R.id.loginPage_Login_button)
+    Button loginButton;
+
+    private String email;
+    private String password;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
--- app/src/main/java/com/android/geeksNotNerds_Makearo/Authentication/Login/MVP/LoginContract.java
+++ app/src/main/java/com/android/geeksNotNerds_Makearo/Authentication/Login/MVP/LoginContract.java
@@ -1,4 +1,10 @@
 package com.android.geeksNotNerds_Makearo.Authentication.Login.MVP;
 
 public class LoginContract {
+    interface view{
+
+    }
+    interface presenter{
+
+    }
 }
